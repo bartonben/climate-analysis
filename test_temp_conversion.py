a16e14d829eaa913c507c554f1bedb97e5cffd6b
vibishna <vibishna.balagopal@gmail.com>
2016-09-01 14:39:36 +0100

    added assert keyword

--- test_temp_conversion.py
+++ test_temp_conversion.py
@@ -2,8 +2,8 @@ from temp_conversion import fahr_to_celsius
 from temp_conversion import fahr_to_kelvin
 
 def test_fahr_cels_abs_zero_input():
-	fahr_to_celsius(-460) == -273
+	assert fahr_to_celsius(-460) == -273
 
 def test_fahr_kelvin_abs_zero_input():
-	fahr_to_kelvin(-460) == 0.0
+	assert fahr_to_kelvin(-460) == 0.0
 	
