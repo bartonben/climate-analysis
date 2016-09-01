ac170f72f702e389eecb550853e0b29fb8e8a9cb
vibishna <vibishna.balagopal@gmail.com>
2016-09-01 14:34:24 +0100

    added absolute zero fahrenheit entry

---
+++ test_temp_conversion.py
@@ -0,0 +1,9 @@
+from temp_conversion import fahr_to_celsius
+from temp_conversion import fahr_to_kelvin
+
+def test_fahr_cels_abs_zero_input():
+	fahr_to_celsius(-460) == -273
+
+def test_fahr_kelvin_abs_zero_input():
+	fahr_to_kelvin(-460) == 0.0
+	
